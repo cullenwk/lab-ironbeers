9a5d5f03cc0b1e4ba96541e5e277230b3f635580
cullen will <cullenwill@cullens-MacBook-Pro.local>
2021-11-10 18:49:31 +0100

    done

--- app.js
+++ app.js
@@ -22,4 +22,28 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {    
+
+  punkAPI.getBeers()
+    .then(beers => { 
+      res.render('beers.hbs' ,{beers})
+    })
+    .catch(error => {
+      console.log(error)
+    });
+
+
+});
+
+app.get('/random-beer', (req, res) => {
+  
+  punkAPI.getRandom()
+  .then(randomBeer => {
+    res.render('random-beer.hbs', {randomBeer});
+  })
+  .catch(error => console.log(error));
+  
+  
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
